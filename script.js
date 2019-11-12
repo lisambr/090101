fba240817601e78ffcf9b363c270f0b82ff44160
lisambr <lisa6068@stud.kea.dk>
2019-11-12 10:38:21 +0100

    starting JS

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function showElements(jsElement) {
     parentElement.appendChild(myCopy);
 
 }
+
+//1 FETCH data from database
+
+fetch("http://iesdesigner.eu/wordpress/wp-json/wp/v2/music?_embed")
+    .then(res => res.json())
+    .then(function (data) {
+        data.forEach(buildCategory)
+        getProducts();
+    })
+
+
+function buildCategory(data) {
+    const section = document.createElement("section");
+    const header = document.createElement("h1");
+    header.textContent = data;
+    section.setAttribute("id", data)
+    section.appendChild(header);
+    document.querySelector("main").appendChild(section);
+}
+
+function getProducts() {
+    fetch("https://kea-alt-del.dk/t5/api/productlist")
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            data.forEach(showDish)
+        })
+}
