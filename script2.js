2849ed7fd5382343bc2d01e8da609e69fb02c587
lisambr <lisa6068@stud.kea.dk>
2019-11-12 12:34:44 +0100

    Calendar Page

---
+++ script2.js
@@ -0,0 +1,15 @@
+showElements()
+
+function showElements(jsElement) {
+    console.log('working')
+    //2 clone template
+    const template = document.querySelector("template").content;
+    const myCopy = template.cloneNode(true);
+    //3 change data
+
+
+    //4 append
+    const parentElement = document.querySelector("main");
+    parentElement.appendChild(myCopy);
+
+}
