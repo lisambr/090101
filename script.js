115d842817fbe88177619d36a8754f79eaf680c6
lisambr <lisa6068@stud.kea.dk>
2019-11-13 12:46:51 +0100

    WHY is JS still not doing its THINGGGGGG

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-window.addEventListener("DOMContentLoaded", init);
+showElements()
+
+function showElements(jsElement) {
+    console.log('working')
+    //2 clone template
+    const template = document.querySelector("template").content;
+    const myCopy = template.cloneNode(true);
+    //3 change data
+
+    //4 append
+    const parentElement = document.querySelector("main");
+    parentElement.appendChild(myCopy);
+
+}
+
+
+//window.addEventListener("DOMContentLoaded", init);
 
 //1 FETCH data from database
 function getFrontpageData() {
@@ -12,12 +28,15 @@ function handleData(myData) {
     myData.forEach(showPost)
 }
 
-//3 CLONE data
+//3A CLONE data
 function showPost(post) {
     const template = document.querySelector(".myTemplate").content;
     const postCopy = template.cloneNode(true);
+    //3B CHANGE Stuff
 
+    const h1 = postCopy.querySelector("h1");
+    h1.textContent = post.title.rendered;
 
-//4 APPEND
+    //4 APPEND
     document.querySelector("#music").appendChild(postCopy)
 }
